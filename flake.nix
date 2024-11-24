556325c5859eaa9c99610ba3a47e4293f098aa67
MattHandzel <39449480+MattHandzel@users.noreply.github.com>
2024-11-24 13:26:39 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,12 +4,13 @@
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }:
-      nixpkgs.config.permittedInsecurePackages = [
-          "electron-28.3.3"
-          ];
+      
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in let pkg = pkgs.callPackage ./package.nix { };
     in {
+nixpkgs.config.permittedInsecurePackages = [
+          "electron-28.3.3"
+          ];
       packages.x86_64-linux.default = pkg;
       packages.x86_64-linux.notion-repackaged = pkg;
     };
