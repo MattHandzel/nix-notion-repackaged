1b065bf1e8ce035feac5954ca326fc762f690a48
MattHandzel <39449480+MattHandzel@users.noreply.github.com>
2024-11-24 13:32:37 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,20 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      # Overlay to modify permittedInsecurePackages
-      myOverlay = self: super: {
-        nixpkgs.config.permittedInsecurePackages = [
-          "electron-28.3.3"
-        ];
-      };
-      
-      pkgs = import nixpkgs {
-        overlays = [ myOverlay ];  # Applying overlay
-        system = "x86_64-linux";
-      };
-      pkg = pkgs.callPackage ./package.nix { };
-    in {
-      packages.x86_64-linux.default = pkg;
-      packages.x86_64-linux.notion-repackaged = pkg;
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.permittedInsecurePackages = [
+        "electron-28.3.3"
+      ];
     };
+    pkg = pkgs.callPackage ./package.nix { };
+  in {
+    packages.x86_64-linux.default = pkg;
+    packages.x86_64-linux.notion-repackaged = pkg;
+  };
 }
