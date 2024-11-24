ac5c7bd4b4dddd24b553397195738708701bde2d
MattHandzel <39449480+MattHandzel@users.noreply.github.com>
2024-11-24 13:17:11 -0600

    Update flake.nix
    i have no idea what I am doing

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }:
+      nixpkgs.config.permittedInsecurePackages = [
+          "electron-28.3.3"
+          ];
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in let pkg = pkgs.callPackage ./package.nix { };
     in {
